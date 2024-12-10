7dfd359f20c3b193805acfb65ff7119c6b517d18
Tommaso Dal Sasso <tommaso.dalsasso@gmail.com>
2024-12-10 17:06:02 +0100

    Add binary link to command line
    Homebrew offers the option of specifying a binary that will be linked
    to the homebrew directory during installation.
    
    Given the recent introduction of a cli interface in Pharo Launcher 3.2
    it makes sense to create a link to the executable.

--- Casks/pharo-launcher.rb
+++ Casks/pharo-launcher.rb
@@ -1,14 +1,16 @@
 cask "pharo-launcher" do
-  arch arm: "arm64",
+  arch arm:   "arm64",
        intel: "x64"
-       
+
   version "3.2"
-  sha256 arm: "d5fa272df66498880315e3ce791dd178ab6f4078aa0fd95a9ee8f2758d5deb59",
+  sha256 arm:   "d5fa272df66498880315e3ce791dd178ab6f4078aa0fd95a9ee8f2758d5deb59",
          intel: "da6a2c5a3a237fcfc085ce664795e6faf586cf25c2b95aee6f3160a0a4ae3786"
 
   url "https://files.pharo.org/pharo-launcher/#{version}/PharoLauncher-#{version}-#{arch}.dmg"
   name "Pharo Launcher"
-  desc "The fastest way to get a working Pharo environment."
+  desc "Fastest way to get a working Pharo environment"
   homepage "http://pharo.org/download"
+
   app "PharoLauncher.app"
+  binary "#{appdir}/PharoLauncher.app/Contents/Resources/pharo-launcher"
 end
